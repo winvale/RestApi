21f012da12e14e4fc6ef6d84cc4a3ce66179e6f1
winvale <harledvalencia01@gmail.com>
2022-12-08 00:59:08 -0500

    pendiete vd 369

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = function () {
-  router.get("/", (req, res) => {
-    res.send("prueba");
-  });
-  router.get("/nosotros", (req, res) => {
-    res.send("nosotros");
-  });
+  //   router.get("/", (req, res) => {
+  //     res.send("prueba");
+  //   });
+  //   router.get("/nosotros", (req, res) => {
+  //     res.send("nosotros");
+  //   });
 
   return router;
 };

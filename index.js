284916a5adc0627c583c56f274299cd85f502c84
winvale <harledvalencia01@gmail.com>
2022-12-08 00:57:41 -0500

    creando rutas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const routes = require("./routes");
 
 //crear el servidor
 const app = express();
+
+//Rutas de la app
+app.use("/", routes());
 
 //puerto
 app.listen(5000);
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = function () {
+  router.get("/", (req, res) => {
+    res.send("prueba");
+  });
+  router.get("/nosotros", (req, res) => {
+    res.send("nosotros");
+  });
+
+  return router;
+};

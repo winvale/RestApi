6c1a240624b9a5668e52a3cb7795b50eb34dc085
winvale <harledvalencia01@gmail.com>
2022-12-08 20:04:13 -0500

    agrego cliente por post a bd

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -1,6 +1,15 @@
 const Clientes = require("../models/Clientes");
 
 //Agregar nuevo cliente
-exports.nuevoCliente = async (req, res) => {
-  console.log(req.body);
+exports.nuevoCliente = async (req, res, next) => {
+  const clientes = new Clientes(req.body);
+
+  try {
+    //almacenar el cliente
+    await clientes.save();
+    res.json({ mensaje: "se agrego un nuevo cliente" });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
 };

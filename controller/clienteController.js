49dfc5fa0a01aebc1d1cbb806b1aaac3c4e62e7e
winvale <harledvalencia01@gmail.com>
2022-12-08 20:12:16 -0500

    metodo get clientes

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -13,3 +13,17 @@ exports.nuevoCliente = async (req, res, next) => {
     next();
   }
 };
+
+//Mostrar clientes
+exports.mostrarClientes = async (req, res, next) => {
+  const clientes = new Clientes(req.body);
+
+  try {
+    //Mostrar clientes
+    const clientes = await Clientes.find({}); // si es basio traer clientes
+    res.json(clientes);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,10 @@ const router = express.Router();
 const clienteController = require("../controller/clienteController");
 
 module.exports = function () {
-  //   router.get("/", (req, res) => {
-  //     res.send("prueba");
-  //   });
-  //   router.get("/nosotros", (req, res) => {
-  //     res.send("nosotros");
-  //   });
-
   //Agreagar nuevos clientes via post
   router.post("/clientes", clienteController.nuevoCliente);
+
+  //obtener clientes
+  router.get("/clientes", clienteController.mostrarClientes);
   return router;
 };
